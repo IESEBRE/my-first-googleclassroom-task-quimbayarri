006755c28ceac291584b8af5f1c1b45a2d6fc5b5
quimbayarri <quimbayarri@iesebre.com>
2023-09-19 13:15:20 +0200

    He afegit el primer projecte Java del curs

---
+++ PrimerProjecte/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Vaig a mostrar per la pantalla el misatge "Hello World"
+        //Shortcut del System.out.println --> sout
+        System.out.println("Hello World");
+        System.out.println("Segona linea de text");
+        System.out.print("Me dic Quim");
+        System.out.println(" i soc alumne de Santi");
+    }
+}
